5c050a80068a0d23b1a199e94bb404549165b8df
Matthew Killeen <58102276+MattKilleen@users.noreply.github.com>
2023-05-03 16:25:39 -0400

    Add files via upload

--- digitPerceptron.py
+++ digitPerceptron.py
@@ -105,7 +105,6 @@ for i in range(len(x_test_raw)):
 
 num_classes = 10
 
-
 def cross_validation_split(dataset, n_folds):
     dataset_split = list()
     dataset_copy = list(dataset)
@@ -131,17 +130,17 @@ def evaluate_algorithm(dataset, algorithm, n_folds, *args):
     scores = list()
     for fold in folds:
         train_set = list(folds)
-    train_set.remove(fold)
-    train_set = sum(train_set, [])
-    test_set = list()
-    for row in fold:
-        row_copy = list(row)
-    test_set.append(row_copy)
-    row_copy[-1] = None
-    predicted = algorithm(train_set, test_set, *args)
-    actual = [row[-1] for row in fold]
-    accuracy = accuracy_metric(actual, predicted)
-    scores.append(accuracy)
+        train_set.remove(fold)
+        train_set = sum(train_set, [])
+        test_set = list()
+        for row in fold:
+            row_copy = list(row)
+            test_set.append(row_copy)
+            row_copy[-1] = None
+            predicted = algorithm(train_set, test_set, *args)
+            actual = [row[-1] for row in fold]
+            accuracy = accuracy_metric(actual, predicted)
+            scores.append(accuracy)
     return scores
 
 
