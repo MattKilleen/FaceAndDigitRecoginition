0826ba5de17725b3f09371a32029d63ff9dd2410
Matthew Killeen <58102276+MattKilleen@users.noreply.github.com>
2023-05-03 14:27:47 -0400

    Add files via upload

--- facePerceptron.py
+++ facePerceptron.py
@@ -99,10 +99,8 @@ for i in range(len(x_test_raw)):
     x_test[i] = x_test_raw[i].flatten()
 
 ### YOUR CODE TO SET UP MODEL STRUCTURE HERE
-myPerceptron = perceptron.PerceptronClassifier([0,1,2,3,4,5,6,7,8,9], 10)
 
 # Record Training Start Time
 start_time = time.time()
 
 ### YOUR TRAIN AND TEST CODE HERE
-myPerceptron.train(rawTrainingData, trainingLabels)
